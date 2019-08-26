2d2ff6397d3019fd3a329aa0b09bd62fd81150a5
Michael Woerister <michaelwoerister@posteo>
2019-08-26 11:32:14 +0200

    Add timings for non-debug builds.

--- run-benches.sh
+++ run-benches.sh
@@ -9,7 +9,7 @@ cargo build
 benchmerge=$root_dir/benchmerge/target/debug/benchmerge
 
 # install the cargo-benchcmp tool
-cargo install -f cargo-benchcmp
+# cargo install -f cargo-benchcmp
 
 # run non-PGO benchmarks
 rm -rf $root_dir/non-pgo-timings
@@ -31,39 +31,88 @@ done
 cd $root_dir/non-pgo-timings
 $benchmerge
 
+# run PGO benchmarks with higher pre-inlining threshold
+#
+# (1) run benchmarks with instrumentation enabled
+cd $root_dir/regex
+cargo clean
+
+rm -rf $root_dir/regex-samples-threshold-150
+mkdir -p $root_dir/regex-samples-threshold-150
+
+cd $root_dir/regex/bench
+
+for i in {1..4}
+do
+    echo "PGO Instrumentation pass $i of 4 (higher pre-inlining threshold)"
+    RUSTFLAGS="-Cprofile-generate=$root_dir/regex-samples-threshold-150 -Cllvm-args=-preinline-threshold=150" ./run rust > /dev/null
+done
+
+# (2) merge the profiling data
+llvm-profdata merge -o $root_dir/regex-samples-threshold-150/merged.profdata $root_dir/regex-samples-threshold-150
+
+# (3) run benchmarks with PGO applied
+rm -rf $root_dir/pgo-timings-threshold-150
+mkdir -p $root_dir/pgo-timings-threshold-150
+
+for i in {1..20}
+do
+    echo "PGO benchmark pass $i of 20 (higher pre-inlining threshold)"
+    RUSTFLAGS="-Cprofile-use=$root_dir/regex-samples-threshold-150/merged.profdata -Cllvm-args=-preinline-threshold=150" ./run rust > $root_dir/pgo-timings-threshold-150/timings-`date +%s`.txt
+done
+
+# merge the benchmark results from the PGO benchmarks
+cd $root_dir/pgo-timings-threshold-150
+$benchmerge
+
+
+
+
+
+
+
 # run PGO benchmarks
 #
 # (1) run benchmarks with instrumentation enabled
 cd $root_dir/regex
 cargo clean
 
-rm -rf $root_dir/regex-samples
-mkdir -p $root_dir/regex-samples
+rm -rf $root_dir/regex-samples-threshold-75
+mkdir -p $root_dir/regex-samples-threshold-75
 
 cd $root_dir/regex/bench
 
-for i in {1..20}
+for i in {1..4}
 do
-    echo "PGO Instrumentation pass $i of 20"
-    RUSTFLAGS="-Cprofile-generate=$root_dir/regex-samples" ./run rust > /dev/null
+    echo "PGO Instrumentation pass $i of 4"
+    RUSTFLAGS="-Cprofile-generate=$root_dir/regex-samples-threshold-75" ./run rust > /dev/null
 done
 
 # (2) merge the profiling data
-llvm-profdata merge -o $root_dir/regex-samples/merged.profdata $root_dir/regex-samples
+llvm-profdata merge -o $root_dir/regex-samples-threshold-75/merged.profdata $root_dir/regex-samples-threshold-75
 
 # (3) run benchmarks with PGO applied
-rm -rf $root_dir/pgo-timings
-mkdir -p $root_dir/pgo-timings
+rm -rf $root_dir/pgo-timings-threshold-75
+mkdir -p $root_dir/pgo-timings-threshold-75
 
 for i in {1..20}
 do
     echo "PGO benchmark pass $i of 20"
-    RUSTFLAGS="-Cprofile-use=$root_dir/regex-samples/merged.profdata" ./run rust > $root_dir/pgo-timings/timings-`date +%s`.txt
+    RUSTFLAGS="-Cprofile-use=$root_dir/regex-samples-threshold-75/merged.profdata" ./run rust > $root_dir/pgo-timings-threshold-75/timings-`date +%s`.txt
 done
 
 # merge the benchmark results from the PGO benchmarks
-cd $root_dir/pgo-timings
+cd $root_dir/pgo-timings-threshold-75
 $benchmerge
+
+
+
+
+
+
+
+
+
 
 # run PGO benchmarks with pre-inlining pass disabled
 #
@@ -76,9 +125,9 @@ mkdir -p $root_dir/regex-samples-no-preinlining
 
 cd $root_dir/regex/bench
 
-for i in {1..20}
+for i in {1..4}
 do
-    echo "PGO Instrumentation pass $i of 20 (no pre-inlining)"
+    echo "PGO Instrumentation pass $i of 4 (no pre-inlining)"
     RUSTFLAGS="-Cprofile-generate=$root_dir/regex-samples-no-preinline -Zdisable-instrumentation-preinliner" ./run rust > /dev/null
 done
 
@@ -99,12 +148,14 @@ done
 cd $root_dir/pgo-timings-no-preinline
 $benchmerge
 
-echo "COMPARISON - Regular PGO vs PGO without pre-inlining pass"
-cargo benchcmp $root_dir/pgo-timings/merged-timings.txt $root_dir/pgo-timings-no-preinline/merged-timings.txt | tee $root_dir/comparison-pgo-pre-inlining-vs-no-pre-inlining.txt
 
-echo "COMPARISON - Regular Non-PGO vs Regular PGO (=with pre-inlining pass)"
-cargo benchcmp $root_dir/non-pgo-timings/merged-timings.txt $root_dir/pgo-timings/merged-timings.txt | tee $root_dir/comparison-no-pgo-vs-pgo.txt
 
-echo "COMPARISON - Regular Non-PGO vs PGO *without* pre-inlining pass"
-cargo benchcmp $root_dir/non-pgo-timings/merged-timings.txt $root_dir/pgo-timings-no-preinline/merged-timings.txt | tee $root_dir/comparison-no-pgo-vs-pgo-no-pre-inlining.txt
+# echo "COMPARISON - Regular PGO vs PGO without pre-inlining pass"
+# cargo benchcmp $root_dir/pgo-timings/merged-timings.txt $root_dir/pgo-timings-no-preinline/merged-timings.txt | tee $root_dir/comparison-pgo-pre-inlining-vs-no-pre-inlining.txt
+
+# echo "COMPARISON - Regular Non-PGO vs Regular PGO (=with pre-inlining pass)"
+# cargo benchcmp $root_dir/non-pgo-timings/merged-timings.txt $root_dir/pgo-timings/merged-timings.txt | tee $root_dir/comparison-no-pgo-vs-pgo.txt
+
+# echo "COMPARISON - Regular Non-PGO vs PGO *without* pre-inlining pass"
+# cargo benchcmp $root_dir/non-pgo-timings/merged-timings.txt $root_dir/pgo-timings-no-preinline/merged-timings.txt | tee $root_dir/comparison-no-pgo-vs-pgo-no-pre-inlining.txt
 
